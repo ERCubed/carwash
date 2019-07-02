11438d10949f761702c679a1b58079ed24737793
Eric Roberts <eric@ercubed.com>
2019-07-02 14:10:46 -0500

    Main vehicle and wash controllers; needs request specs

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -6,8 +6,15 @@ class VehiclesController < ApplicationController
     @vehicles = Vehicle.order(:make)
   end
 
-  def show
+  def show; end
 
+  # TODO: Add in strong params so we can just call @vehicle.update(vehicle_params)
+  def update; end
+
+  # So sorry to see you go!
+  # TODO: Tie this in with the Account to deactivate it, especially if it's the only vehicle on the account. 
+  def destroy
+    @vehicle.destroy
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   resources :vehicles, param: :license_plate, defaults: {format: :json}
+  post 'wash/:license_plate', to: 'wash#create', defaults: {format: :json}
 end
